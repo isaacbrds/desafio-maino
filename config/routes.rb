16dfa8451e7a1b9741df084cb3eaf376993ed994
isaacbrds <isaac.brigido@gmail.com>
2021-11-25 17:23:54 -0300

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
   mount Sidekiq::Web => '/sidekiq'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
